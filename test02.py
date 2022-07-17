0ddfb057bd0913f11d703d729f883cc49bb1525f
Ralston <51337039+whitestarliew@users.noreply.github.com>
2022-07-17 10:06:43 +0800

    Update test02.py

--- test02.py
+++ test02.py
@@ -1,2 +1,14 @@
-def nice():
-  print ("This is nice!")
+conversion_usdollar = 4.4
+currency_unit = "MYR"
+
+def newnumber(amount_of_dollar):
+    if amount_of_dollar > 0 :
+        return f" You have {amount_of_dollar} ,that is equal to {conversion_usdollar * amount_of_dollar} {currency_unit} "
+    else:
+        return "You are wrong"
+
+user_input = input ("Please key in your amount\n")
+user_input_number = int(user_input)
+
+calculated_amount = newnumber(user_input_number)
+print(calculated_amount)
