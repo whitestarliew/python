7cb3a8024af20e8e0cc032dacac1b39d1d990c41
whitestarliew <whitestarliew@gmail.com>
2024-02-29 15:51:12 +0800

    update

--- sample_go/sample.go
+++ sample_go/sample.go
@@ -4,8 +4,16 @@ import "fmt"
 
 func main() {
 	var conferenceName = "Go conference"
-	const 
-	fmt.Println("Hello World", conferenceName)
-	fmt.Println("lol")
+	const priceTicket = 20
+	var remainingTickets = 34
+	fmt.Printf("Hello World %v", conferenceName)
+	fmt.Printf("we get total of %v", priceTicket)
+	fmt.Println("this is worth RM", priceTicket, "we still have", remainingTickets, "Tickets")
 
+	var userName string
+	var ticketmoney int
+	userName = "Ronaldo"
+	ticketmoney = 99
+	fmt.Println(userName)
+	fmt.Printf("Hey %v ,this is the new price of the ticket, which is RM%v", userName, ticketmoney)
 }
