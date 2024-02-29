5c763742d4dbaeff57f9f665a6c2b61827b28898
whitestarliew <whitestarliew@gmail.com>
2024-02-29 16:42:02 +0800

    update

--- installation01.sh
+++ installation01.sh
@@ -21,15 +21,23 @@ else
   echo "Failed to update package lists. Please check your network connection or system logs for errors."
   exit 1  # Stop the script if update fails
 fi
-# Add Docker repository
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-# Update package lists again
-sudo apt -y update 
-# Install Docker engine and CLI
-sudo apt install -y docker-ce docker-ce-cli docker-compose
 
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+#New docker installation 
+# Add Docker's official GPG key:
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 # Start Docker service
 sudo systemctl start docker
 
