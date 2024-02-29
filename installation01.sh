b80928bfb7ac07203681859f55c01db23f66c6f5
whitestarliew <whitestarliew@gmail.com>
2024-02-29 17:24:45 +0800

    update

--- installation01.sh
+++ installation01.sh
@@ -26,6 +26,7 @@ for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker c
 
 #New docker installation 
 # Add Docker's official GPG key:
+sudo apt-get update
 sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
@@ -37,7 +38,7 @@ echo \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 # Start Docker service
 sudo systemctl start docker
 
