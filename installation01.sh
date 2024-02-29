082ce6b6226d900fa31b4858c35b60a2d8613ab0
whitestarliew <whitestarliew@gmail.com>
2024-02-29 17:54:44 +0800

    update

--- installation01.sh
+++ installation01.sh
@@ -73,8 +73,10 @@ echo "Docker installation complete!"
 #############################Install Jenkins ####################################### 
 # Install Java runtime for Jenkins
 sudo apt-get install -y openjdk-17-jre
-sudo apt-get install -y jenkins
 
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get install -y jenkins
 # Install Jenkins
 if apt install -y jenkins 2>/dev/null; then  # Suppress standard output to avoid cluttering
   echo "Jenkins installation complete!"
